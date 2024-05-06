3b8661be45c04d9e58d749bdf7ad4a3a5d691abf
yuanze31 <1538981620@qq.com>
2024-05-06 14:28:02 +0800

    sync

---
+++ Class1.cs
@@ -0,0 +1,37 @@
+﻿using HarmonyLib;
+
+namespace Remove25tAbsorbLimiit
+{
+    public class Patches
+    {
+        [HarmonyPatch(typeof(ElementSplitterComponents))]
+        [HarmonyPatch("CanFirstAbsorbSecond")]
+        public class Patch_Absorb
+        {
+            public static bool Prefix(
+                HandleVector<int>.Handle first,
+                HandleVector<int>.Handle second,
+                ref bool __result
+            )
+            {
+                if (
+                    first == HandleVector<int>.InvalidHandle
+                    || second == HandleVector<int>.InvalidHandle
+                )
+                {
+                    __result = false;
+                    return false;
+                }
+                ElementSplitter data = GameComps.ElementSplitters.GetData(first);
+                ElementSplitter data2 = GameComps.ElementSplitters.GetData(second);
+
+                __result =
+                    data.primaryElement.ElementID == data2.primaryElement.ElementID
+                    && data.primaryElement.Units + data2.primaryElement.Units < 100000f
+                    && !data.kPrefabID.HasTag(GameTags.MarkedForMove)
+                    && !data2.kPrefabID.HasTag(GameTags.MarkedForMove);
+                return false;
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# ONI_Remove25tAbsorbLimiit
+# 移除碎片合成重量限制
+---
+- 修改限制由25t到100t
+- 本来想做真无限制的，但是发现好像会吞掉超过100t的部分
+- 而且有溢出风险，就这样吧
+## Steam社区页面：https://steamcommunity.com/sharedfiles/filedetails/?id=3240628520
